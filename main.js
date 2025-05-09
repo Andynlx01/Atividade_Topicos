941556d095d36fdae72c9072ab1448fe3d0d7e61
Andynlx01 <162315449+Andynlx01@users.noreply.github.com>
2025-05-09 13:31:47 -0300

    update
    adição do script e efeito no texto

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 function selecionarCasa(elemento) {
-    // Remove a seleção atual
     const casas = document.querySelectorAll('.casa');
     casas.forEach(casa => casa.classList.remove('selecionada'));
   
-    // Adiciona a nova seleção
     elemento.classList.add('selecionada');
   }
-
   function toggleEvento(elemento) {
     elemento.classList.toggle('ativo');
   }
-
+  document.addEventListener("DOMContentLoaded", () => {
+    const historia = document.querySelector('.historia');
+    if (historia) {
+      setTimeout(() => {
+        historia.classList.add('visivel');
+      }, 300);
+    }
+  });
